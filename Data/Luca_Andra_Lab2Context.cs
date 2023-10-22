9ffe3ecab78bc00fd1d929c0a52af60e1e77e6f1
AndraGabrielaLuca <117577158+AndraGabrielaLuca@users.noreply.github.com>
2023-10-22 16:06:50 +0300

    Add project files.

---
+++ Data/Luca_Andra_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Luca_Andra_Lab2.Models;
+
+namespace Luca_Andra_Lab2.Data
+{
+    public class Luca_Andra_Lab2Context : DbContext
+    {
+        public Luca_Andra_Lab2Context (DbContextOptions<Luca_Andra_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Luca_Andra_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Luca_Andra_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
